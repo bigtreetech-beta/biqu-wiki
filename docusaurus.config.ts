db68eaf3db0e1ec1a92f1f22b8c4711e16e03482
Neko.vecter <36069884+Neko-vecter@users.noreply.github.com>
2025-12-27 01:04:06 -0500

    Update site URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://www.biquwiki.com',
+  url: 'https://bigtreetech-beta.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/biqu-wiki',
