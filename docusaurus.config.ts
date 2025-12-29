728c406c0868fc8ee19ffb63314b1e771a1971ee
Neko-vecter <vecterfang@icloud.com>
2025-12-29 06:22:54 -0500

    build: update config url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://bigtreetech-beta.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/biqu-wiki',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -37,9 +37,11 @@ const config: Config = {
     localeConfigs: {
       "zh-Hans": {
         label: '简体中文',
+        baseUrl: '/zh'
       },
       "en": {
         label: 'English',
+        baseUrl: '/en',
         htmlLang: 'en-US',
       },
     }
