4365ebc1e48fcff40eb387c9f492795d3b145c4c
bttwiki-team <bttwiki-team@biqu3d.com>
2025-12-30 00:08:53 -0500

    static: update social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.webp',
     colorMode: {
       respectPrefersColorScheme: true,
       defaultMode: 'dark',
